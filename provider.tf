0699906badccbbd76b5cec898e145658ed0b115b
Shadow-Lord123 <152615325+Shadow-Lord123@users.noreply.github.com>
2025-06-20 12:20:05 +0100

    Pipeline

--- provider.tf
+++ provider.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     azurerm = {
@@ -10,7 +9,18 @@ terraform {
   required_version = ">= 1.0"
 }
 
+# Declare input variables so Terraform knows about them
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
+
+# Provider uses the variables
 provider "azurerm" {
   features {}
-}
 
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
