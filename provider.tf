4995f979c339c27e7b6d5cc49f8d26cfa66e1e39
Shadow-Lord123 <152615325+Shadow-Lord123@users.noreply.github.com>
2025-06-20 16:10:30 +0100

    Pipeline #9

--- provider.tf
+++ provider.tf
@@ -9,18 +9,13 @@ terraform {
   required_version = ">= 1.0"
 }
 
-# Declare input variables so Terraform knows about them
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
-
 # Provider uses the variables
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  Client_ID       = var.Client_ID
+  Client_Secret   = var.Client_Secret
+  Subscription_ID = var.Subscription_ID
+  Tenant_ID       = var.Tenant_ID
 }
+
+
--- variable.tf
+++ variable.tf
@@ -7,3 +7,24 @@ variable "location_name" {
   description = "Location Name for VPC"
   type        = string
 }
+
+variable "Client_ID" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "Client_Secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "Subscription_ID" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "Tenant_ID" {
+  description = "Azure Tenant ID"
+  type        = string
+}
