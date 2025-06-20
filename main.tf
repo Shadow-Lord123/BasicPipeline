b40d61446ae3a64b400b0359d9b6ab1c15096661
Shadow-Lord123 <152615325+Shadow-Lord123@users.noreply.github.com>
2025-06-20 11:11:22 +0100

    module error fix

--- main.tf
+++ main.tf
@@ -4,9 +4,3 @@ module "app_functions" {
   rg_name = var.rg_name
   location_name = var.location_name     
 }
-
-module "provider" {
-  source = "./module"
-  rg_name = var.rg_name
-  location_name = var.location_name
-}
