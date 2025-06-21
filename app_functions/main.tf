5adf80fad44355d20f4ae5119efee246b0435e2b
Shadow-Lord123 <152615325+Shadow-Lord123@users.noreply.github.com>
2025-06-21 19:31:42 +0100

    Pipeline #25

--- app_functions/main.tf
+++ app_functions/main.tf
@@ -1,21 +1,22 @@
 
+
 resource "azurerm_resource_group" "example" {
-  name     = var.rg_name  
+  name     = "DevRG"
   location = var.location_name
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "kritagyafuncappsa" 
-  resource_group_name      = var.rg_name
-  location                 = var.location_name
+  name                     = "kritagyafuncappsa"
+  resource_group_name      = azurerm_resource_group.example.name   
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_service_plan" "example" {
   name                = "azure-functions-test-service-plan"
-  location            = var.location_name
-  resource_group_name = var.rg_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name        
 
   sku_name = "S1"
   os_type  = "Windows"
@@ -23,8 +24,8 @@ resource "azurerm_service_plan" "example" {
 
 resource "azurerm_windows_function_app" "example" {
   name                       = "kritagyafunction"
-  location                   = var.location_name
-  resource_group_name        = var.rg_name
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name 
   service_plan_id            = azurerm_service_plan.example.id
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
@@ -34,8 +35,8 @@ resource "azurerm_windows_function_app" "example" {
 
 resource "azurerm_kubernetes_cluster" "example" {
   name                = "kritagyaaks1"
-  location            = var.location_name
-  resource_group_name = var.rg_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name        
   dns_prefix          = "kritagyadns"
 
   default_node_pool {
