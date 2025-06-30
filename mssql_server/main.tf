bd4b0173f5a727ed6e7ddcf6e70490482a70cb51
Shadow-Lord123 <152615325+Shadow-Lord123@users.noreply.github.com>
2025-06-30 11:18:30 +0100

    Push

--- mssql_server/main.tf
+++ mssql_server/main.tf
@@ -1,19 +1,51 @@
 
 resource "azurerm_mssql_server" "example" {
   name                         = "kritagyasqlserver"
-  resource_group_name          = var.dev_rg_name
-  location                     = var.location_name
+  resource_group_name          = "dev-resource-group"
+  location                     = "East US"
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "thisIsKat11"
   minimum_tls_version          = "1.2"
 
+  public_network_access_enabled        = true
+  outbound_network_restriction_enabled = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
   azuread_administrator {
     login_username = "AzureAD Admin"
-    object_id      = var.object_id
+    object_id      = "11111111-1111-1111-1111-111111111111"
   }
 
   tags = {
-    environment = "production"
+    env   = "prod"
+    team  = "devops"
+    proj  = "sql"
+    tf    = "true"
+    comp  = "yes"
+    crit  = "yes"
+    patch = "yes"
+    upd   = "2025-06-30"
   }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_my_ip" {
+  name              = "allow-my-ip"
+  server_id         = azurerm_mssql_server.example.id
+  start_ip_address  = "86.24.253.5"  # <--- Replace with your IP
+  end_ip_address    = "86.24.253.5"
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_azure" {
+  name              = "allow-azure-services"
+  server_id         = azurerm_mssql_server.example.id
+  start_ip_address  = "0.0.0.0"
+  end_ip_address    = "0.0.0.0"
 }
